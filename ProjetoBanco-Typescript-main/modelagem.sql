30d31b46bbdd8551393d2890873022024db02d3c
Mírian Menezes <81202092+Icognita@users.noreply.github.com>
2021-09-25 17:34:05 -0300

    Add files via upload
    Enviei a parte das classe

---
+++ ProjetoBanco-Typescript-main/modelagem.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE soulBanco
+USE soulBanco;
+
+DROP TABLE transacao;
+DROP TABLE login_cliente;
+DROP TABLE conta;
+DROP TABLE clientes;
+
+CREATE TABLE IF NOT EXISTS clientes(
+id_cliente INT NOT NULL AUTO_INCREMENT,
+nome_cliente VARCHAR(11) NOT NULL,
+endereco_cliente VARCHAR(30) NOT NULL,
+cpf_cliente VARCHAR (11) NOT NULL,
+PRIMARY KEY(id_cliente)
+);
+
+CREATE TABLE IF NOT EXISTS conta(
+agencia_cliente VARCHAR(11) NOT NULL,
+saldo_cliente FLOAT NOT NULL,
+cliente_id INT NOT NULL,
+conta_cliente VARCHAR(10) NOT NULL,
+PRIMARY KEY(conta_cliente),
+FOREIGN KEY (cliente_id) REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE IF NOT EXISTS login_cliente(
+id_login INT NOT NULL AUTO_INCREMENT,
+senha VARCHAR(10) NOT NULL,
+usuario VARCHAR(10) NOT NULL,
+PRIMARY KEY(id_login),
+FOREIGN KEY (usuario) REFERENCES conta(conta_cliente)
+);
+
+CREATE TABLE IF NOT EXISTS transacao(
+id_transacao INT NOT NULL AUTO_INCREMENT,
+data_transacao DATE NOT NULL,
+destinatario VARCHAR (30) NOT NULL,
+valor_transacao FLOAT NOT NULL,
+cliente_conta VARCHAR(10) NOT NULL,
+PRIMARY KEY(id_transacao),
+FOREIGN KEY (cliente_conta) REFERENCES conta(conta_cliente)
+);
+
+INSERT INTO clientes(nome_cliente, endereco_cliente, cpf_cliente) VALUES ('maria', 'rua bom jesus, 54', '12345678954');
+INSERT INTO clientes(nome_cliente, endereco_cliente, cpf_cliente) VALUES ('jose', 'rua do menino, 54', '23456789123');
+
+INSERT INTO conta(agencia_cliente, saldo_cliente, cliente_id, conta_cliente) VALUES ('001', 1000, 1, '12345');
+INSERT INTO conta(agencia_cliente, saldo_cliente, cliente_id, conta_cliente) VALUES ('002', 500, 2, '23456');
+
+INSERT INTO login_cliente(senha, usuario) VALUES ('1234', '12345');
+INSERT INTO login_cliente(senha, usuario) VALUES ('2345', '23456');
+
+INSERT INTO transacao (data_transacao, destinatario, valor_transacao, cliente_conta) VALUES ('2021-09-24', 'amelia', 200, '12345');
+INSERT INTO transacao (data_transacao, destinatario, valor_transacao, cliente_conta) VALUES ('2021-09-23', 'paulo', 230, '23456');
+
+
